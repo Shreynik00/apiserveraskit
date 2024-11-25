2bb726c4bb27a21fbedb7b57c40f0d615b1b6ec4
Shreynik00 <jainsy@rknec.edu>
2024-11-26 03:22:05 +0530

    Update displaydata.js

--- displaydata.js
+++ displaydata.js
@@ -274,7 +274,7 @@ app.get('/user/:username', async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: 'User not found.' });
         }
-        res.json({ username: user.username, email: user.email });
+        res.json({ username: user.username, about: user.about, skills : user.skills });
     } catch (error) {
         console.error('Error fetching user details:', error);
         res.status(500).json({ message: 'Internal server error.' });
