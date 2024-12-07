07a55033d65ccfbe0bbf88b6e69f77616dfea4d6
Shreynik00 <jainsy@rknec.edu>
2024-12-08 01:37:13 +0530

    Update displaydata.js

--- displaydata.js
+++ displaydata.js
@@ -125,7 +125,7 @@ io.on('connection', (socket) => {
 });
 
 //to redirect to sendmessage page from reciver end
-app.post('/chatredirector', async (req, res) => {
+app.get('/chatredirector', async (req, res) => {
     const { username } = req.body;
 
     // Validate input
